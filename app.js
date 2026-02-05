173750221d6beb0f5bf0dfe6b56b1c34b03af2e9
bdm2013 <bdm2013@yahoo.com>
2026-02-05 09:28:52 -0600

    Update app.js

--- app.js
+++ app.js
@@ -762,7 +762,7 @@ songForm?.addEventListener("submit", async (e) => {
 });
 
 /* ---------- Playback Integration (Apple Music + YouTube Music) ---------- */
-const YT_API_KEY = "AIzaSyDE4RjoYFYADsg3YVkJg6AUniegOtM7f3c";
+const YT_API_KEY = "AIzaSyBnIwX-F4YoxXW4gatIwvJD-NF23XcdwaI";
 /**
  * Build YouTube queries based on genre.
  * - Tv/Movie/Kids: ONLY "Title Artist"
@@ -1736,6 +1736,7 @@ refreshArchiveList();
 renderRecent();
 
 renderLastImportMeta();
+
 
 
 
